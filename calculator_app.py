90bfd565dfdd06a417438633fbc192b22a0e9221
Miserable- <61861617+topmikeymike@users.noreply.github.com>
2024-01-26 16:55:16 +0800

    Update calculator_app.py

--- calculator_app.py
+++ calculator_app.py
@@ -16,8 +16,8 @@ def header():
 def main_content():
 
     # Load data from CSV files
-    file_path_negeri_daerah = r"data\Negeri_Daerah.csv"
-    file_model = r"data\modelv1.csv"
+    file_path_negeri_daerah = "data/Negeri_Daerah.csv"
+    file_model = "data/modelv1.csv"
     # file_path_strata = r"C:\Users\michael.igo\Documents\Coding\calculator_exp\data\LU_STRATA.csv"
     # data_strata = load_data(file_path_strata)
     data_negeri_daerah = load_data(file_path_negeri_daerah)
