07e8d4b5ae480ef3fba75a6b19af61ce1fdf94d5
Miserable- <61861617+topmikeymike@users.noreply.github.com>
2024-01-29 18:06:11 +0800

    Update calculator_app.py

--- calculator_app.py
+++ calculator_app.py
@@ -9,23 +9,66 @@ def load_data(file_path):
     return data
 
 
-def header():
-    st.header("Expenditure Calculator 🌟")
-    st.markdown(
-        """
-        <div style="text-align: justify; font-size: 20px; line-height: 1.6; color: #444;">
-            <p>Feeling the pinch in the middle of the month?</p>
-            <p>Wondering how your monthly expenses should look?</p>
-            <p>Introducing the Expenditure Prediction Calculator.</p>
-            <p style="font-weight: bold;">Let our AI model calculate your expenses based on your inputs.</p>
-        </div>
-        """,
-        unsafe_allow_html=True
-    ) 
+def header(selected_language):
+    if selected_language == "English":
+        logo_image2 = "images/logo2.png" # Replace with the actual path to your logo image
+        logo_image = "images/logo.png" # Replace with the actual path to your logo image
+        logo_image1 = "images/logo1.png" # Replace with the actual path to your logo image
+        
+        # Create three columns for each image
+        col1, col2, col3 = st.columns(3)
+
+        # Display the logo images in horizontal order
+        col1.image(logo_image2, width=100)
+        col2.image(logo_image, width=150)
+        col3.image(logo_image1, width=150)
+    
+        # Display the logo image
+        # st.image(logo_image2, width=100)
+        # st.image(logo_image, width=100)
+        # st.image(logo_image1, width=100)
+        st.header("Expenditure Calculator 🌟")
+        st.markdown(
+            """
+            <div style="text-align: justify; font-size: 20px; line-height: 1.6; color: #444;">
+                <p>Wondering how your monthly expenses should look?</p>
+                <p>Now, you can find out the value of your expenses with <b>4 steps.</b></p>
+                <p style="font-weight: bold;">Let our Artificial Intelligence (AI) model calculate your expenses based on your inputs.</p>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+    else:
+        st.header("Kalkulator Perbelanjaan 🌟")
+        st.markdown(
+            """
+            <div style="text-align: justify; font-size: 20px; line-height: 1.6; color: #444;">
+                <p>Tertanya-tanya berapakah sepatutnya perbelanjaan bulanan anda?</p>
+                <p>Kini, anda boleh mengetahui nilai perbelanjaan anda dengan <b>4 Langkah</b> sahaja.</p>
+                <p style="font-weight: bold;">Kalkulator ini akan mengira perbelanjaan anda berdasarkan maklumat yang anda berikan dengan penggunaan model <i>Artificial Intelligence (AI)</i> kami.</p>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
 
 def select_language():
-    selected_language = st.radio("Select Language", ["English", "Malay"])
+    selected_language = st.radio("Select Language", ["English", "Malay"], format_func=str)
+
+    # Apply custom CSS to move the radio button to the right
+    st.markdown(
+        """
+        <style>
+        .css-2trqyj {
+            justify-content: flex-end;
+            display: flex-end;
+        }
+        </style>
+        """,
+        unsafe_allow_html=True
+    )
+
     return selected_language
+
 
 def get_language_dict(selected_language):
     if selected_language == "English":
@@ -40,7 +83,18 @@ def get_language_dict(selected_language):
             "input_bil_wargaEmas": "Number of Senior Citizens:",
             "input_bil_kanak2": "Number of Children:",
             "input_hh_income": "Household Income:",
-            "predicted_expenditure": "Predicted Expenditure for Household (RM):"
+            "predicted_expenditure": "Predicted Expenditure for Household (RM):",
+            "step_1": "Step 1: Your Location",
+            "detail_step1": "Please select the state you are currently residing in and whether it is in an urban or rural area",
+            "step_2": "Step 2: Head of Household Information",
+            "detail_step2": "Please select your Gender and Ethnicity",
+            "step_3": "Step 3: Your Household Income",
+            "detail_step3": "Please enter your total monthly income of all your household members. This includes income from rent, investments or other transfers.",
+            "step_4": "Step 4: Your Household Information",
+            "detail_step4": "Please enter your number of household as well as the number of elderly people, the number of children and the number of disabled people.",
+            "footer": "Copyright © 2024 Michael/Khairul. All rights reserved",
+            "sumber": "Source:",
+            "source": "Department of Statistics Malaysia, Household Income and Expenditure Survey (HIES) 2022"
         }
     else:
         # Default to Malay if language is not English
@@ -55,8 +109,19 @@ def get_language_dict(selected_language):
             "input_bil_wargaEmas": "Bilangan Warga Emas:",
             "input_bil_kanak2": "Bilangan Kanak-Kanak:",
             "input_hh_income": "Pendapatan Isi Rumah:",
-            "predicted_expenditure": "Jangkaan Perbelanjaan untuk Isi Rumah (RM):"
-        }
+            "predicted_expenditure": "Perbelanjaan Yang Diramalkan untuk Isi Rumah (RM):",
+            "step_1": "Langkah 1: Lokasi Anda",
+    	    "detail_step1": "Sila pilih Daerah tempat tinggal anda dan sama ada Bandar atau Luar Bandar.",
+            "step_2": "Langkah 2: Maklumat Ketua Isi Rumah",
+    	    "detail_step2": "Sila pilih Jantina dan Etnik Anda.", 
+            "step_3": "Langkah 3: Pendapatan Isi Rumah Anda",
+    	    "detail_step3": "Sila masukkan jumlah pendapatan bulanan semua ahli isi rumah anda. Ini termasuk pendapatan daripada sewa, pelaburan atau pindahan lain.", 
+            "step_4": "Langkah 4: Maklumat Isi Rumah Anda",
+    	    "detail_step4": "Sila masukkan bilangan isi rumah Anda serta bilangan warga emas, bilangan kanak-kanak dan bilangan OKU.",
+            "footer": "Hakcipta © 2024 Michael/Khairul. Hakcipta terpelihara",
+            "sumber": "Sumber:",
+            "source": "Jabatan Perangkaan Malaysia, Survei Pendapatan dan Perbelanjaan Isi Rumah (HIES) 2022 "
+         }
     
     return language
 
@@ -80,8 +145,9 @@ def main_content(language_dict):
     # st.dataframe(data_model)
     
     # Display the options in a single-select dropdown for Negeri
+    st.header(language_dict["step_1"])
+    st.write(language_dict["detail_step1"])
     selected_negeri = st.selectbox(language_dict["select_negeri"], ["Selangor"] + list(data_negeri_daerah['NEGERI'].unique()))
-
 
     # Filter Daerah options based on selected Negeri
     daerah_options = list(data_negeri_daerah[data_negeri_daerah['NEGERI'] == selected_negeri]['DAERAH'].unique())
@@ -117,6 +183,10 @@ def main_content(language_dict):
     # Display the coefficient for the selected Daerah
     # st.write(f"Coefficient for {selected_strata}: {round(selected_coefficient, 12)}") 
     
+    st.markdown("<br>", unsafe_allow_html=True)
+    
+    st.header(language_dict["step_2"])
+    st.write(language_dict["detail_step2"])
     # Display the options in a single-select dropdown for Jantina
     selected_jantina = st.selectbox(language_dict["select_jantina"], ["Lelaki"] + list(data_negeri_daerah['JANTINA'].dropna().unique()))
     
@@ -144,51 +214,50 @@ def main_content(language_dict):
 
     # Display the coefficient for the selected Daerah
     # st.write(f"Coefficient for {selected_etnik}: {round(selected_coefficient, 12)}") 
-
+    st.markdown("<br>", unsafe_allow_html=True)
     
-    # # Declare coefficient
-    # coefficient_bil_ir = item_coeff_dict.get('Bil IR', default_feature)
-    # coefficient_kanak2 = item_coeff_dict.get('Bil Kanak-kanak', default_feature)
-    # coefficient_warga_emas = item_coeff_dict.get('Bil Warga Emas', default_feature)
-    # coefficient_oku = item_coeff_dict.get('Bil OKU', default_feature)
-    # coefficient_hh_income = item_coeff_dict.get('HH_Income', default_feature)
-    # constant_term = item_coeff_dict.get('Constant', default_feature)
+    #declare coefficient hhincome
+    coefficient_hh_income = round(item_coeff_dict.get('HH_Income', default_feature), 12)
     
-    # # Declare coefficient with 12 decimal points
+    st.header(language_dict["step_3"])
+    st.write(language_dict["detail_step3"])
+    user_input_hh_income = st.number_input(language_dict["input_hh_income"], value=0.0, step=0.01, format="%.2f", min_value=0.0)
+    ln_user_input_hh_income = np.log(user_input_hh_income) if user_input_hh_income > 0 else 0.0
+    ln_coefficient_hh_income = coefficient_hh_income * ln_user_input_hh_income
+    # coefficient_hh_income= coefficient_hh_income * user_input_hh_income
+    # st.write("User Input for LN Household Income:", round(ln_user_input_hh_income, 12))
+    # st.write("Coefficient for LN Household Income:", round(ln_coefficient_hh_income, 12))
+    st.markdown("<br>", unsafe_allow_html=True)
+    
+    
+    st.header(language_dict["step_4"])
+    st.write(language_dict["detail_step4"])
+    # Declare coefficient with 12 decimal points
     coefficient_bil_ir = round(item_coeff_dict.get('Bil IR', default_feature), 12)
     coefficient_kanak2 = round(item_coeff_dict.get('Bil Kanak-kanak', default_feature), 12)
     coefficient_warga_emas = round(item_coeff_dict.get('Bil Warga Emas', default_feature), 12)
     coefficient_oku = round(item_coeff_dict.get('Bil OKU', default_feature), 12)
-    coefficient_hh_income = round(item_coeff_dict.get('HH_Income', default_feature), 12)
     constant_term = round(item_coeff_dict.get('Constant', default_feature), 12)
-    
-
     
    # Get user input for an integer
     user_input_bil_air = st.number_input(language_dict["input_bil_air"], value=0, step=1, min_value=0)
     coefficient_bil_ir= coefficient_bil_ir * user_input_bil_air
     # st.write("Coefficient for Bil IR:", round(coefficient_bil_ir, 12))
     
-    user_input_bil_oku = st.number_input(language_dict["input_bil_oku"], value=0, step=1, min_value=0, max_value=user_input_bil_air)
-    coefficient_oku= coefficient_oku * user_input_bil_oku
-    # st.write("Coefficient for Bil OKU:", round(coefficient_oku, 12))
-
-    user_input_bil_wargaEmas = st.number_input(language_dict["input_bil_wargaEmas"], value=0, step=1, min_value=0, max_value=user_input_bil_air - user_input_bil_oku)
+    user_input_bil_wargaEmas = st.number_input(language_dict["input_bil_wargaEmas"], value=0, step=1, min_value=0, max_value=user_input_bil_air )
     coefficient_warga_emas= coefficient_warga_emas * user_input_bil_wargaEmas
     # st.write("Coefficient for Bil Warga Emas:", round(coefficient_warga_emas, 12))
 
-    user_input_bil_kanak2 = st.number_input(language_dict["input_bil_kanak2"], value=0, step=1, min_value=0, max_value=user_input_bil_air - user_input_bil_oku - user_input_bil_wargaEmas)
+    user_input_bil_kanak2 = st.number_input(language_dict["input_bil_kanak2"], value=0, step=1, min_value=0, max_value=user_input_bil_air - user_input_bil_wargaEmas)
     coefficient_kanak2= coefficient_kanak2 * user_input_bil_kanak2
     # st.write("Coefficient for Bil Kanak-kanak:", round(coefficient_kanak2, 12))
-
-    user_input_hh_income = st.number_input(language_dict["input_hh_income"], value=0.0, step=0.01, format="%.2f", min_value=0.0)
-    # Calculate the natural logarithm (ln) of the user input
-    ln_user_input_hh_income = np.log(user_input_hh_income) if user_input_hh_income > 0 else 0.0
-    ln_coefficient_hh_income = coefficient_hh_income * ln_user_input_hh_income
-    # coefficient_hh_income= coefficient_hh_income * user_input_hh_income
-    # st.write("User Input for LN Household Income:", round(ln_user_input_hh_income, 12))
-    # st.write("Coefficient for LN Household Income:", round(ln_coefficient_hh_income, 12))
-
+    
+    user_input_bil_oku = st.number_input(language_dict["input_bil_oku"], value=0, step=1, min_value=0, max_value=user_input_bil_air)
+    coefficient_oku= coefficient_oku * user_input_bil_oku
+    # st.write("Coefficient for Bil OKU:", round(coefficient_oku, 12))
+    
+    st.markdown("<br>", unsafe_allow_html=True)
+    
     # Coefficients for the selected features
     coefficient_daerah = item_coeff_dict.get(selected_daerah, default_feature)
     coefficient_strata = item_coeff_dict.get(selected_strata, default_feature)
@@ -215,8 +284,19 @@ def main_content(language_dict):
     # st.write("Sum :", round(exp_selected_coefficient, 12))
 
     # Display the exponentiated coefficient for the selected_daerah
-    st.write(f"{language_dict['predicted_expenditure']} {round(exp_selected_coefficient, 2)}")
+    st.write(
+        f"<p style='font-size: 22px; font-weight: bold;'>{language_dict['predicted_expenditure']} {round(exp_selected_coefficient, 2)}</p>",
+        unsafe_allow_html=True
+    )
 
+    st.markdown(
+        f"""
+        <div style="text-align:left;">
+            <p style="font-size: 12px; color: grey;">{language_dict['sumber']} {language_dict['source']}</p>
+        </div>
+        """,
+        unsafe_allow_html=True
+    )
     
     # Calculate e raised to the power of sum_coefficient
     result = math.exp(sum_coefficient)
@@ -226,14 +306,14 @@ def main_content(language_dict):
     # st.write(f"e^{sum_coefficient} = {formatted_result}")
 
     
-def footer():
-    # Footer with copyright
+def footer(language_dict):
     st.markdown(
-        """
+        f"""
         <div style="text-align:center; padding: 10px;">
-            <p style="font-size: 12px; color: grey;">Copyright © 2024 Michael/Khairul. All rights reserved.</p>
-            <p style="font-size: 12px; color: grey;">bank in dulu</p>
+            <p style="font-size: 12px; color: grey;">{language_dict['footer']}</p>
+            <p style="font-size: 12px; color: grey;">Ver.1.0</p>
         </div>
         """,
         unsafe_allow_html=True
     )
+
