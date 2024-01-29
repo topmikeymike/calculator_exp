8a791a9d323665f4da9bb69d3443c3fb1729ce28
Miserable- <61861617+topmikeymike@users.noreply.github.com>
2024-01-29 15:30:54 +0800

    Update main.app.py

--- main.app.py
+++ main.app.py
@@ -1,11 +1,24 @@
 import streamlit as st
-from calculator_app import header, main_content, footer
-
+from calculator_app import header, main_content, footer, select_language, get_language_dict
 
 def main():
     
     header()
-    main_content()
+    # Add a line break
+    st.markdown("<br>", unsafe_allow_html=True)
+    
+    # Call select_language to get the selected language
+    selected_language = select_language()
+    
+    # Call get_language_dict with the selected language
+    language_dict = get_language_dict(selected_language)
+    
+    
+    main_content(language_dict)
+    
+    # Add a line break
+    st.markdown("<br>", unsafe_allow_html=True)
+    
     footer()
 
 if __name__ == "__main__":
