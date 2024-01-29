c8d94108a6101f564c0dac07f21d08fb1fec7268
Miserable- <61861617+topmikeymike@users.noreply.github.com>
2024-01-29 15:30:33 +0800

    Update calculator_app.py

--- calculator_app.py
+++ calculator_app.py
@@ -10,7 +10,7 @@ def load_data(file_path):
 
 
 def header():
-    st.title("Expenditure Calculator 🌟")
+    st.header("Expenditure Calculator 🌟")
     st.markdown(
         """
         <div style="text-align: justify; font-size: 20px; line-height: 1.6; color: #444;">
@@ -23,8 +23,46 @@ def header():
         unsafe_allow_html=True
     ) 
 
+def select_language():
+    selected_language = st.radio("Select Language", ["English", "Malay"])
+    return selected_language
 
-def main_content():
+def get_language_dict(selected_language):
+    if selected_language == "English":
+        language = {
+            "select_negeri": "Select State",
+            "select_daerah": "Select District",
+            "select_strata": "Select Strata",
+            "select_jantina": "Select Gender",
+            "select_etnik": "Select Ethnicity",
+            "input_bil_air": "Number of Households:",
+            "input_bil_oku": "Number of OKU:",
+            "input_bil_wargaEmas": "Number of Senior Citizens:",
+            "input_bil_kanak2": "Number of Children:",
+            "input_hh_income": "Household Income:",
+            "predicted_expenditure": "Predicted Expenditure for Household (RM):"
+        }
+    else:
+        # Default to Malay if language is not English
+        language = {
+            "select_negeri": "Pilih Negeri",
+            "select_daerah": "Pilih Daerah",
+            "select_strata": "Pilih Strata",
+            "select_jantina": "Pilih Jantina",
+            "select_etnik": "Pilih Etnik",
+            "input_bil_air": "Bilangan Isi Rumah:",
+            "input_bil_oku": "Bilangan OKU:",
+            "input_bil_wargaEmas": "Bilangan Warga Emas:",
+            "input_bil_kanak2": "Bilangan Kanak-Kanak:",
+            "input_hh_income": "Pendapatan Isi Rumah:",
+            "predicted_expenditure": "Perbelanjaan Yang Diramalkan untuk Isi Rumah (RM):"
+        }
+    
+    return language
+
+
+def main_content(language_dict):
+    
 
     # Load data from CSV files
     file_path_negeri_daerah = "data/Negeri_Daerah.csv"
@@ -42,12 +80,12 @@ def main_content():
     # st.dataframe(data_model)
     
     # Display the options in a single-select dropdown for Negeri
-    selected_negeri = st.selectbox("Negeri", ["Selangor"] + list(data_negeri_daerah['NEGERI'].unique()))
+    selected_negeri = st.selectbox(language_dict["select_negeri"], ["Selangor"] + list(data_negeri_daerah['NEGERI'].unique()))
 
 
     # Filter Daerah options based on selected Negeri
     daerah_options = list(data_negeri_daerah[data_negeri_daerah['NEGERI'] == selected_negeri]['DAERAH'].unique())
-    selected_daerah = st.selectbox("Daerah", [""] + daerah_options)
+    selected_daerah = st.selectbox(language_dict["select_daerah"], [""] + daerah_options)
 
     # daerah_coeff_dict = dict(zip(daerah_list, coefficients))
     item_coeff_dict = dict(zip(data_model['Item'], data_model['Coefficient']))
@@ -66,7 +104,7 @@ def main_content():
 
     
     # Display the options in a single-select dropdown for Strata
-    selected_strata = st.selectbox("Strata", ["Bandar"] + list(data_negeri_daerah['STRATA'].dropna().unique()))
+    selected_strata = st.selectbox(language_dict["select_strata"], ["Bandar"] + list(data_negeri_daerah['STRATA'].dropna().unique()))
     
      # Handle Bandar as a special case
     if selected_strata == "Bandar":
@@ -80,7 +118,7 @@ def main_content():
     # st.write(f"Coefficient for {selected_strata}: {round(selected_coefficient, 12)}") 
     
     # Display the options in a single-select dropdown for Jantina
-    selected_jantina = st.selectbox("Jantina KIR(Ketua Isi Rumah)", ["Lelaki"] + list(data_negeri_daerah['JANTINA'].dropna().unique()))
+    selected_jantina = st.selectbox(language_dict["select_jantina"], ["Lelaki"] + list(data_negeri_daerah['JANTINA'].dropna().unique()))
     
      # Handle Bandar as a special case
     if selected_jantina == "Lelaki":
@@ -94,7 +132,7 @@ def main_content():
     # st.write(f"Coefficient for {selected_jantina}: {round(selected_coefficient, 12)}") 
     
     # Display the options in a single-select dropdown for Etnik
-    selected_etnik = st.selectbox("Etnik KIR(Ketua Isi Rumah)", ["Melayu"] + list(data_negeri_daerah['KUMP_ETNIK'].dropna().unique()))
+    selected_etnik = st.selectbox(language_dict["select_etnik"], ["Melayu"] + list(data_negeri_daerah['KUMP_ETNIK'].dropna().unique()))
     
     # Handle Bandar as a special case
     if selected_etnik == "Melayu":
@@ -107,43 +145,6 @@ def main_content():
     # Display the coefficient for the selected Daerah
     # st.write(f"Coefficient for {selected_etnik}: {round(selected_coefficient, 12)}") 
 
-    # # Get user input for an integer
-    # user_input_bil_air = st.number_input("Bilangan Isi Rumah:", key="bil_air", value=0, step=1)
-
-    # # Display the entered integer
-    # st.write("Bilangan Isi Rumah:", user_input_bil_air)
-
-    # # Get user input for an integer
-    # user_input_bil_oku = st.number_input("Bilangan OKU:", key="bil_oku", value=0, step=1)
-
-    # # Display the entered integer
-    # st.write("Bilangan OKU:", user_input_bil_oku)
-
-    # # Get user input for an integer
-    # user_input_bil_wargaEmas = st.number_input("Bilangan Warga Emas:", key="bil_wargaEmas", value=0, step=1)
-
-    # # Display the entered integer
-    # st.write("Bilangan Warga Emas:", user_input_bil_wargaEmas)
-
-    # # Get user input for an integer
-    # user_input_bil_kanak2 = st.number_input("Bilangan Kanak-Kanak:", key="bil_kanak2", value=0, step=1)
-
-    # # Display the entered integer
-    # st.write("Bilangan Kanak-Kanak:", user_input_bil_kanak2)
-
-    # # Get user input for income (double)
-    # user_input_hh_income = st.number_input("Pendapatan Isi Rumah:", key="hh_income", value=0.0, step=0.01, format="%.2f")
-
-    # # Display the entered income with a default value
-    # st.write("Pendapatan Isi Rumah: RM {:.2f}".format(user_input_hh_income))
-
- 
-     # Numeric coefficients
-    # coefficient_bil_air = 0.100
-    # coefficient_bil_kanak2 = -0.0051
-    # coefficient_bil_wargaEmas = -0.033
-    # coefficient_bil_oku = -0.090 
-    # coefficient_hh_income = 0.0000370972249842198
     
     # # Declare coefficient
     # coefficient_bil_ir = item_coeff_dict.get('Bil IR', default_feature)
@@ -164,23 +165,23 @@ def main_content():
 
     
    # Get user input for an integer
-    user_input_bil_air = st.number_input("Bilangan Isi Rumah:", value=0, step=1, min_value=0)
+    user_input_bil_air = st.number_input(language_dict["input_bil_air"], value=0, step=1, min_value=0)
     coefficient_bil_ir= coefficient_bil_ir * user_input_bil_air
     # st.write("Coefficient for Bil IR:", round(coefficient_bil_ir, 12))
     
-    user_input_bil_oku = st.number_input("Bilangan OKU:", value=0, step=1, min_value=0, max_value=user_input_bil_air)
+    user_input_bil_oku = st.number_input(language_dict["input_bil_oku"], value=0, step=1, min_value=0, max_value=user_input_bil_air)
     coefficient_oku= coefficient_oku * user_input_bil_oku
     # st.write("Coefficient for Bil OKU:", round(coefficient_oku, 12))
 
-    user_input_bil_wargaEmas = st.number_input("Bilangan Warga Emas:", value=0, step=1, min_value=0, max_value=user_input_bil_air - user_input_bil_oku)
+    user_input_bil_wargaEmas = st.number_input(language_dict["input_bil_wargaEmas"], value=0, step=1, min_value=0, max_value=user_input_bil_air - user_input_bil_oku)
     coefficient_warga_emas= coefficient_warga_emas * user_input_bil_wargaEmas
     # st.write("Coefficient for Bil Warga Emas:", round(coefficient_warga_emas, 12))
 
-    user_input_bil_kanak2 = st.number_input("Bilangan Kanak-Kanak:", value=0, step=1, min_value=0, max_value=user_input_bil_air - user_input_bil_oku - user_input_bil_wargaEmas)
+    user_input_bil_kanak2 = st.number_input(language_dict["input_bil_kanak2"], value=0, step=1, min_value=0, max_value=user_input_bil_air - user_input_bil_oku - user_input_bil_wargaEmas)
     coefficient_kanak2= coefficient_kanak2 * user_input_bil_kanak2
     # st.write("Coefficient for Bil Kanak-kanak:", round(coefficient_kanak2, 12))
 
-    user_input_hh_income = st.number_input("Pendapatan Isi Rumah:", value=0.0, step=0.01, format="%.2f", min_value=0.0)
+    user_input_hh_income = st.number_input(language_dict["input_hh_income"], value=0.0, step=0.01, format="%.2f", min_value=0.0)
     # Calculate the natural logarithm (ln) of the user input
     ln_user_input_hh_income = np.log(user_input_hh_income) if user_input_hh_income > 0 else 0.0
     ln_coefficient_hh_income = coefficient_hh_income * ln_user_input_hh_income
@@ -212,9 +213,10 @@ def main_content():
     # Exponentiate the coefficient
     exp_selected_coefficient = np.exp(round(sum_coefficient, 12))
     # st.write("Sum :", round(exp_selected_coefficient, 12))
-    
+
     # Display the exponentiated coefficient for the selected_daerah
-    st.write(f"Predicted Expenditure for Household (RM): {round(exp_selected_coefficient, 2)}")
+    st.write(f"{language_dict['predicted_expenditure']} {round(exp_selected_coefficient, 2)}")
+
     
     # Calculate e raised to the power of sum_coefficient
     result = math.exp(sum_coefficient)
