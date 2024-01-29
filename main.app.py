f7e66d724dbc284c294c41d2678cc50de5806555
Miserable- <61861617+topmikeymike@users.noreply.github.com>
2024-01-29 18:06:32 +0800

    Update main.app.py

--- main.app.py
+++ main.app.py
@@ -3,23 +3,24 @@ from calculator_app import header, main_content, footer, select_language, get_la
 
 def main():
     
-    header()
+    with st.sidebar:
+        # Call select_language to get the selected language
+        selected_language = select_language()
+
+        # Call get_language_dict with the selected language
+        language_dict = get_language_dict(selected_language)
+
+    header(selected_language)
+    
     # Add a line break
     st.markdown("<br>", unsafe_allow_html=True)
-    
-    # Call select_language to get the selected language
-    selected_language = select_language()
-    
-    # Call get_language_dict with the selected language
-    language_dict = get_language_dict(selected_language)
-    
     
     main_content(language_dict)
     
     # Add a line break
     st.markdown("<br>", unsafe_allow_html=True)
     
-    footer()
+    footer(language_dict)
 
 if __name__ == "__main__":
     main()
